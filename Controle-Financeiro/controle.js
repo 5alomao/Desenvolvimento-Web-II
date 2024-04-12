cee4d7b87595d08c2485c8617e3d0e0a9f2f3bdd
5alomao <17413319608@campus.machado.mch>
2024-04-12 08:22:52 -0300

    Parte 02 - Controle Financeiro - soma receita,despesa,lista transacao

--- Controle-Financeiro/controle.js
+++ Controle-Financeiro/controle.js
@@ -5,6 +5,10 @@ const valueInput = document.getElementById("montante");
 
 const balancoH1 = document.getElementById("balanco");
 
+const receitaP = document.getElementById("din-positivo");
+const despesaP = document.getElementById("din-negativo");
+
+const transacoesUL = document.getElementById("transacoes");
 
 form.addEventListener("submit", event => {
     // Não submete o formulário - Retira funcionalidade padrão do elemento
@@ -28,20 +32,44 @@ form.addEventListener("submit", event => {
     const transacao = {
         id: parseInt(Math.random() * 100000),
         desc: descTransacao,
-        value: valueTransacao
+        valor: parseFloat(valueTransacao)
     };
 
     somaAoSaldo(transacao);
+    somaReceitaDespesa(transacao);
+    addTransacaoAoDOM(transacao);
 
     descInput.value = '';
     valueInput.value = '';
 });
 
 function somaAoSaldo(transacao) {
-    // Recuperar Saldo
-    // Pegar Saldo, Remover o R$
     let valorBalanco = balancoH1.innerHTML.trim();
     valorBalanco = valorBalanco.replace("R$", "");
+
     valorBalanco = parseFloat(valorBalanco);
-    // TO DO ---
+    valorBalanco += transacao.valor;
+    balancoH1.innerHTML = `R$${valorBalanco.toFixed(2)}`;
+}
+
+function somaReceitaDespesa(transacao) {
+
+    const elemento = transacao.valor > 0 ? receitaP : despesaP;
+    const substituir = transacao.valor > 0 ? "+ R$" : "- R$";
+    let valor = elemento.innerHTML.replace(substituir, "");
+    valor = parseFloat(valor);
+    valor += Math.abs(transacao.valor);
+    elemento.innerHTML = `${substituir}${valor.toFixed(2)}`;
+}
+
+function addTransacaoAoDOM(transacao) {
+
+    const cssClass = transacao.valor > 0 ? 'positivo' : 'negativo';
+    const currency = transacao.valor > 0 ? '+R$' : "-R$";
+    const liElementStr = `${transacao.desc} <span>${currency} ${Math.abs(transacao.valor)}</span><button class="delete-btn">X</button>`;
+
+    const liElement = document.createElement('li');
+    liElement.innerHTML = liElementStr;
+    liElement.classList.add(cssClass);
+    transacoesUL.appendChild(liElement);
 }
